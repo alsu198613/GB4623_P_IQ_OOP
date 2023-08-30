531f5c7c62dea7aed6492c247ed7e4c3b32e9eed
alsu198613 <alsu198613@gmail.com>
2023-08-30 08:25:51 +0300

    added sem 2

---
+++ 1 семинар/VendingMachine/src/Domen/Place.java
@@ -0,0 +1,26 @@
+package Domen;
+
+public class Place {
+private int row;
+private int col;
+
+public int getRow() {
+return row;
+}
+
+public void setRow(int row) {
+this.row = row;
+}
+
+public int getCol() {
+return col;
+}
+
+public void setCol(int col) {
+this.col = col;
+}
+
+public boolean isEmpty(){
+    return true;
+}
+}
---
+++ 1 семинар/VendingMachine/src/Services/Holder.java
@@ -0,0 +1,19 @@
+package Services;
+
+public class Holder {
+
+List <Product>;
+List <place>;
+boolean condition; // состояние (держит товар или нет)
+
+
+
+public void release(place place, ){
+
+};// метод продажи (выдачи товара)
+public void error (){
+
+} // метод что делать если ошибка
+
+
+}
